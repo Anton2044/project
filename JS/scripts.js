516b41dc80057fdc4c1fa16a0e13ead317a73bc3
Anton <a.1592@mail.ru>
2020-05-16 18:18:58 +0300

    first commit

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// console.log(`http://someurl.com/${categorys}/5`); //интерполяция!
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr + " " + decr);
+
+console.log(2 + 3 == 5 || 2 + 1 == 5);
---
+++ ads.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+int a;
+cin >> a;
+for (int i = 0; i < a; i++)
+{
+    for (int j= 0; j < a; j++)
+    {
+        cout << i + 1 <<" ";
+    }
+    cout << endl;
+    
+}
+}
